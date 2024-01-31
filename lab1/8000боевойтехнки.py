993e4d85025d2fd3ed095f33a808d3d802fc74ac
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2024-01-31 15:00:38 +0300

    хуйня

--- lab1/8000боевойтехнки.py
+++ lab1/8000боевойтехнки.py
@@ -10,10 +10,19 @@ CELL_SIZE = 50
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 SEA_BLUE = (0, 119, 190)
-SHIP_COLORS = [(255, 175, 0), (0, 255, 0), (255, 0, 0), (0, 0, 255)]
-HIT_COLOR = (255, 0, 0)
-SINK_COLOR = (128, 0, 128)  # Фиолетовый цвет для отображения подбитого корабля
+SHIPS_COLOR = (0, 33, 55)
+HIT_COLOR = (128, 0, 128)  # Фиолетовый цвет для отображения попадания
 FONT_SIZE = 20
+
+
+def draw_shot_result(board, row, col, offset=0):
+    if board[row][col] == 'X':
+        pygame.draw.rect(screen, HIT_COLOR, (col * CELL_SIZE + offset, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+    elif board[row][col] == ' ':
+        pygame.draw.rect(screen, BLACK, (col * CELL_SIZE + offset, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+
+
 
 class SeaBattleGame:
     def __init__(self):
@@ -22,6 +31,7 @@ class SeaBattleGame:
         self.player_ships = {'L': 4, 'C': 3, 'D': 2, 'B': 1}
         self.computer_ships = {'L': 4, 'C': 3, 'D': 2, 'B': 1}
         self.computer_hits = set()
+        self.computer_first_shot = False
 
     def place_ships(self, board):
         ship_types = ['L', 'C', 'C', 'D', 'D', 'D', 'B', 'B', 'B', 'B']
@@ -57,48 +67,79 @@ class SeaBattleGame:
             if self.computer_board[guess_row][guess_col] != ' ':
                 ship_type = self.computer_board[guess_row][guess_col]
                 print(f"Congratulations! You hit an enemy {ship_type}!")
+                self.computer_board[guess_row][guess_col] = 'X'  # Помечаем попадание компьютера
+                self.player_board[guess_row][guess_col] = 'H'  # Помечаем попадание игрока
+
                 if ship_type in self.computer_ships:
                     self.computer_ships[ship_type] -= 1
                     self.computer_ships[ship_type] = max(0, self.computer_ships[ship_type])
                     if self.computer_ships[ship_type] == 0:
                         print(f"You sank an enemy {ship_type}!")
-                    self.computer_board[guess_row][guess_col] = 'X'  # Помечаем попадание крестиком
-                else:
-                    print("You already hit this ship.")
             else:
                 print("You missed!")
+                self.computer_board[guess_row][guess_col] = 'M'  # Помечаем промах компьютера
+                self.player_board[guess_row][guess_col] = 'B'  # Помечаем промах игрока
         else:
             print("Invalid move. Click inside the board.")
 
+        draw_shot_result(self.computer_board, guess_row, guess_col)  # в player_turn
+
     def computer_turn(self):
-        # Различные стратегии для компьютерного игрока
-        # В данной реализации просто выбирается случайная клетка, но можно добавить более сложные стратегии
-        guess_row = random.randint(0, 9)
-        guess_col = random.randint(0, 9)
+        if self.computer_hits:
+            x, y = self.computer_hits.pop()
+        else:
+            if not self.computer_first_shot:
+                x, y = self.get_optimal_first_shot()
+                self.computer_first_shot = True
+            else:
+                x, y = self.get_optimal_shot()
 
-        while (guess_row, guess_col) in self.computer_hits:
-            guess_row = random.randint(0, 9)
-            guess_col = random.randint(0, 9)
-
-        if self.player_board[guess_row][guess_col] != ' ':
-            ship_type = self.player_board[guess_row][guess_col]
+        if self.player_board[x][y] != ' ':
+            ship_type = self.player_board[x][y]
             print(f"Oh no! The computer hit your {ship_type}!")
-            if ship_type in self.player_ships:
+            if ship_type != 'M' and ship_type != 'H' and ship_type != 'X':
+                self.player_board[x][y] = 'H'  # Помечаем попадание игрока
                 self.player_ships[ship_type] -= 1
-                self.player_ships[ship_type] = max(0, self.player_ships[ship_type])
-                self.player_board[guess_row][guess_col] = 'X'  # Помечаем попадание крестиком
                 if self.player_ships[ship_type] == 0:
                     print(f"The computer sank your {ship_type}!")
-            else:
-                print("The computer already hit this ship.")
+                    self.computer_hits.clear()
         else:
             print("Phew! The computer missed!")
+            self.player_board[x][y] = 'M'  # Помечаем промах игрока
+
+        draw_shot_result(self.player_board, x, y, WIDTH // 2)  # в computer_turn
+
+    def get_optimal_first_shot(self):
+        # Просто выстрелы по диагонали
+        return random.choice([(i, i) for i in range(10)])
+
+    def get_optimal_shot(self):
+        if self.computer_hits:
+            x, y = self.computer_hits.pop()
+            potential_shots = [
+                (x - 1, y), (x + 1, y),
+                (x, y - 1), (x, y + 1)
+            ]
+            potential_shots = [(i, j) for i, j in potential_shots if
+                               0 <= i < 10 and 0 <= j < 10 and (i, j) not in self.computer_hits]
+            if potential_shots:
+                return random.choice(potential_shots)
+            else:
+                # Если все окружающие клетки были уже проверены, выберем случайную непроверенную клетку
+                unchecked_cells = [(i, j) for i in range(10) for j in range(10) if (i, j) not in self.computer_hits]
+                return random.choice(unchecked_cells)
+        else:
+            # Если попаданий нет, выбираем случайную клетку, не бившуюся ранее
+            unchecked_cells = [(i, j) for i in range(10) for j in range(10) if (i, j) not in self.computer_hits]
+            return random.choice(unchecked_cells)
+
 
 # Инициализация игрового окна
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sea Battle Game")
 
 font = pygame.font.SysFont(None, FONT_SIZE)
+
 
 def draw_board(board, offset, show_ships=True):
     for i in range(10):
@@ -109,15 +150,17 @@ def draw_board(board, offset, show_ships=True):
             if show_ships:
                 ship_type = board[i][j]
                 if ship_type != ' ':
-                    color_index = ord(ship_type) - ord('A')
-                    color = SHIP_COLORS[color_index] if 0 <= color_index < len(SHIP_COLORS) else WHITE
-                    pygame.draw.rect(screen, color, (j * CELL_SIZE + offset, i * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                    pygame.draw.rect(screen, SHIPS_COLOR, (j * CELL_SIZE + offset, i * CELL_SIZE, CELL_SIZE, CELL_SIZE))
             elif board[i][j] == 'X':
-                pygame.draw.rect(screen, SINK_COLOR, (j * CELL_SIZE + offset, i * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                draw_shot_result(board, i, j, offset)  # Перенесено сюда
+
+
+
 
 def draw_text(text, x, y):
     text_surface = font.render(text, True, WHITE)
     screen.blit(text_surface, (x, y))
+
 
 def main():
     game = SeaBattleGame()
